5a4555a2b7b19522ebf8e58c72ff330b43c4bca7
legolover107 <aklegolover@gmail.com>
2026-04-16 08:17:14 -0400

    added assignment

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 
@@ -14,43 +16,70 @@ public class Main {
         return mostMarks[1];
     }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        //BingoPlayer.simpleNumbers = false;
-        BingoPlayer[] players = new BingoPlayer[5];
-        players[0] = new BingoPlayer("Bob");
-        players[1] = new BingoPlayer("Fred");
-        players[2] = new BingoPlayer("Ben");
-        players[3] = new BingoPlayer("Jason");
-        players[4] = new BingoPlayer("Aiden");
+        try {
+            File f = new File("birthdayFile.txt");
+            Scanner scan = new Scanner(f);
+            ArrayList<ArrayList<String>> students = new ArrayList<>();
+            while (scan.hasNextLine()) {
+                ArrayList<String> temp = new ArrayList<>();
+                String str = scan.nextLine();
+                String[] temp1 = str.split(" ");
+                for (String str1:temp1) {
+                    if (str1.equals("II,")) {
 
-        for (BingoPlayer player:players) {
-            player.generateCard();
-        }
-
-        ArrayList<Integer> hopper = new ArrayList<>();
-        for (int i = 1; i <= 75; i++) {
-            hopper.add(i);
-        }
-        String[] BINGO = {"B", "I", "N", "G", "O"};
-        int count = 0;
-        while (count < 75) {
-            int ind = (int)(Math.random() * hopper.size());
-            int num = hopper.get(ind);
-            hopper.remove(ind);
-            System.out.println("Caller:\t" + BINGO[(num-1)/15] + " " + num);
-            for (BingoPlayer player:players) {
-                player.markCard(num);
-            }
-            System.out.println(getMostMarks(players));
-            for (BingoPlayer player:players) {
-                if (player.winnerFound()) {
-                    System.out.println("BINGO: " + player.getName());
-                    player.printCard();
-                    System.exit(0);
+                    } if (str1.endsWith(",")) {
+                        temp.add(str1.substring(0, str1.length() - 1));
+                    } else {
+                        temp.add(str1);
+                    }
                 }
+                students.add(temp);
             }
-            count++;
+            String[] names = new String[students.size()];
+            for (int i = 0; i < students.size(); i++) {
+                names[i] = students.get(i).get(1);
+            }
+            //BingoPlayer.simpleNumbers = false;
+            String[] selectedNames = new String[5];
+            //for (i = 0;)
+            BingoPlayer[] players = new BingoPlayer[5];
+            players[0] = new BingoPlayer();
+            players[1] = new BingoPlayer("Fred");
+            players[2] = new BingoPlayer("Ben");
+            players[3] = new BingoPlayer("Jason");
+            players[4] = new BingoPlayer("Aiden");
+
+            for (BingoPlayer player:players) {
+                player.generateCard();
+            }
+
+            ArrayList<Integer> hopper = new ArrayList<>();
+            for (int i = 1; i <= 75; i++) {
+                hopper.add(i);
+            }
+            String[] BINGO = {"B", "I", "N", "G", "O"};
+            int count = 0;
+            while (count < 75) {
+                int ind = (int)(Math.random() * hopper.size());
+                int num = hopper.get(ind);
+                hopper.remove(ind);
+                System.out.println("Caller:\t" + BINGO[(num-1)/15] + " " + num);
+                for (BingoPlayer player:players) {
+                    player.markCard(num);
+                }
+                System.out.println(getMostMarks(players));
+                for (BingoPlayer player:players) {
+                    if (player.winnerFound()) {
+                        System.out.println("BINGO: " + player.getName());
+                        player.printCard();
+                        System.exit(0);
+                    }
+                }
+                count++;
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: File not found");
         }
-        scan.close();
     }
 }
